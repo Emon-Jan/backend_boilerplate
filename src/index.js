94ff061ab8415a8b5e6a88d916c99815f27658a3
Muktadir Imam Jan <muktadirimam@gmail.com>
2020-09-05 22:46:41 +0600

    nodejs boilerplate

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+import express from "express";
+import createError from "http-errors";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -15,8 +12,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use((req, res, next) => {
+  console.log("Logging");
+  next();
+});
+
+app.use("/", (req, res, next) => {
+  res.send("Hello World!");
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
